5f94eb5407d8f63a88a21a92df20464cfb3fc60b
bozostein <bozosteindev@gmail.com>
2024-07-25 01:41:35 +0300

    Added getPlayerFunction / player's prompt input

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function getComputerChoice() {
     const number = Math.floor(Math.random() * options.length);
     return number;
 }
+
+
+
+// Function to get player's choice by prompt function. 
+// When player writes an option like "rock" getPlayerFunction, takes the input, makes it all lowercase and returns it. 
+
+function getPlayerChoice() {
+    let playerSelection = false;
+    
+    while (playerSelection == false){
+        const selection = prompt("Rock Paper Scissors");
+
+        if (playerSelection == null) {
+            console.log("You've quit the game");
+            return null;
+        }
+
+        const selectionInLower = selection.toLowerCase();
+
+        if (selection.includes(selectionInLower)) {
+            playerSelection = true;
+            return selectionInLower;
+        }
+}   }
+
+
+
+getPlayerChoice()
